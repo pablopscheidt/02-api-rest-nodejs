e58af02a274db8c44da0d511e557451fb2bf0e46
pablopscheidt <nikebryt@gmail.com>
2025-07-24 09:39:06 -0300

    initial commit

---
+++ src/middlewares/check-session-id-exists.ts
@@ -0,0 +1,15 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function checkSessionIdExists(
+  req: FastifyRequest,
+  res: FastifyReply,
+) {
+  const sessionId = req.cookies.sessionId
+
+  if (!sessionId) {
+    return res.status(401).send({
+      error: 'Unauthorized',
+      message: 'Session ID is required to access transactions.',
+    })
+  }
+}
